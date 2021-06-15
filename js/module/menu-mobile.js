c580fb5f62718f3ac01c7d6b0008b4bb8cbe9885
sergio <serginho111level@gmail.com>
2021-06-15 19:52:26 -0300

    testando funcionalidade

--- js/module/menu-mobile.js
+++ js/module/menu-mobile.js
@@ -3,13 +3,14 @@ import initOutsideEvent from './outside-event.js';
 export default function initMenuMobile() {
   const buttonMenu = document.querySelector('[data-menu="button"]');
   const navMenu = document.querySelector('[data-menu="nav"]');
-  const eventos = ['click', 'touchstart'];
+  const active = 'active';
+  const eventos = ['click'];
   function activeMenu() {
-    buttonMenu.classList.toggle('active');
-    navMenu.classList.toggle('active');
+    buttonMenu.classList.toggle(active);
+    navMenu.classList.toggle(active);
     initOutsideEvent(navMenu, buttonMenu, eventos, () => {
-      buttonMenu.classList.remove('active');
-      navMenu.classList.remove('active');
+      buttonMenu.classList.remove(active);
+      navMenu.classList.remove(active);
     });
   }
   eventos.forEach((evento) => {
