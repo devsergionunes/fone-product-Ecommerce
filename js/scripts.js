69f014145fe91093830c02846aba22e5f4910436
sergio <serginho111level@gmail.com>
2021-06-15 19:04:09 -0300

    refatorado o arq req-products e trasnformado em classe

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 import initMenuMobile from './module/menu-mobile.js';
 import initApiShipping from './module/api-shipping.js';
-import initReqProducts from './module/req-product.js';
+import ReqProducts from './module/req-product.js';
 import SetImgs from './module/set-Imagens.js';
 
 initMenuMobile();
 initApiShipping();
-initReqProducts();
+const reqproducts = new ReqProducts('assets/api-product/product.json', '[data-product="imagens"]', '[data-product="imagem"]');
+reqproducts.init();
 const setimgs = new SetImgs('[data-product="imagem"]', '[data-product="imagens"]');
 setimgs.init();
